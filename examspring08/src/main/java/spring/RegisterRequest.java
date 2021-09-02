dee2a0f64c6d8db4680b27b2a2528c6af4f339be
Taemin <greatperson21@naver.com>
2021-09-02 17:22:52 +0900

    [ STUDY ] 스프링 MVC 구현 2

--- examspring08/src/main/java/spring/RegisterRequest.java
+++ examspring08/src/main/java/spring/RegisterRequest.java
@@ -1,33 +1,42 @@
 package spring;
 
+//커맨드 객체
 public class RegisterRequest {
 	private String email;
 	private String password;
 	private String confirmPassword;
 	private String name;
 	
+	public RegisterRequest() {
+		System.out.println("RegisterRequest()");
+	}
+	
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
+		System.out.println("setEmail");
 		this.email = email;
 	}
 	public String getPassword() {
 		return password;
 	}
 	public void setPassword(String password) {
+		System.out.println("setPassword");
 		this.password = password;
 	}
 	public String getConfirmPassword() {
 		return confirmPassword;
 	}
 	public void setConfirmPassword(String confirmPassword) {
+		System.out.println("setConfirmPassword");
 		this.confirmPassword = confirmPassword;
 	}
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
+		System.out.println("setName");
 		this.name = name;
 	}
 	@Override
---
+++ examspring08/src/main/webapp/WEB-INF/views/main.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>메인 페이지</title>
+</head>
+<body>
+<p>환영합니당</p>
+<p><a href="<c:url value="/register/step1"/>">회원 가입</a></p>
+</body>
+</html>
