aa7822f9ba0a1140d57e82f01c5f997fc277d8b7
taemin <greatperson21@naver.com>
2021-09-06 17:06:10 +0900

    [ STUDY ] 개인 공부 AOP 이해하기

---
+++ spring/src/spring/aop/Program.java
@@ -0,0 +1,42 @@
+package spring.aop;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.aop.entity.Exam;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context =
+				//new AnnotationConfigApplicationContext(NewlecDIConfig.class);
+				new ClassPathXmlApplicationContext("spring/aop/setting.xml");
+		
+		Exam exam = (Exam) context.getBean("exam");
+		
+		System.out.printf("totali is %d\n", exam.total());
+		System.out.printf("avg is %f\n", exam.avg());
+		
+		/*
+		 * Exam exam = new NewlecExam(1,1,1,1);
+		 * 
+		 * Exam proxy = (Exam) Proxy.newProxyInstance(NewlecExam.class.getClassLoader(),
+		 * new Class[] {Exam.class}, new InvocationHandler() {
+		 * 
+		 * @Override public Object invoke(Object proxy, Method method, Object[] args)
+		 * throws Throwable {
+		 * 
+		 * long start = System.currentTimeMillis();
+		 * 
+		 * Object result = method.invoke(exam, args);
+		 * 
+		 * long end = System.currentTimeMillis(); String message = (end - start) +
+		 * "ms 시간이 걸렸습니다."; System.out.println(message);
+		 * 
+		 * return result; } } );
+		 */
+		
+	}
+
+}
